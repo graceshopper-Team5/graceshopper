4c78aec2ad51c4ca899aff53e0827451b6855d0c
Jane Balfus <janebalfus@gmail.com>
2021-08-03 13:10:18 -0400

    Product & User reducer files, set up test specs for User thunk

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,11 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import usersReducer from './usersReducer'
+import productsReducer from './productsreducer'
 
-const reducer = combineReducers({ auth })
+
+const reducer = combineReducers({ auth, usersReducer, productsReducer })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
