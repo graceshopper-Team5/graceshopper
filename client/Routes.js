7d8c4f2fd39560ab4eb6119cbf65396f54ee8edc
Samantha Radecki <samantha.radecki@gmail.com>
2021-08-04 16:04:54 -0500

    cart page is able to see all items added & can remove items from cart

--- client/Routes.js
+++ client/Routes.js
@@ -1,34 +1,33 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
-import {me} from './store'
-import Products from './components/Products'
-import SingleProduct from './components/SingleProduct'
-import { Cart } from './components/Cart'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import Home from "./components/Home";
+import { me } from "./store";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
+import Cart from "./components/Cart";
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData()
+    this.props.loadInitialData();
   }
 
   render() {
-    const {isLoggedIn} = this.props
+    const { isLoggedIn } = this.props;
 
     return (
       <div>
         {isLoggedIn ? (
-        // when user is logged in
+          // when user is logged in
           <Switch>
-              <Route path="/cart" component={Cart} />
+            <Route path="/cart" component={Cart} />
             <Route path="/home" component={Home} />
             <Route path="/home" component={Products} />
             <Redirect to="/home" />
-          
           </Switch>
         ) : (
           // when user is not logged in
@@ -37,36 +36,35 @@ class Routes extends Component {
             <Route exact path="/" component={Products} />
             <Route exact path="/products" component={Products} />
             {/* render single product view */}
-            <Route exact path="/products/:id" component={SingleProduct}/>
+            <Route exact path="/products/:id" component={SingleProduct} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-          
           </Switch>
         )}
       </div>
-    )
+    );
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+      dispatch(me());
+    },
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
