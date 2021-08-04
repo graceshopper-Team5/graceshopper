2f3d7c4bede309ecfc874bd1c38435730428c973
Samantha Radecki <samantha.radecki@gmail.com>
2021-08-04 14:49:58 -0500

    fixed route

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,61 +3,59 @@ import { connect } from "react-redux";
 import { getProduct } from "../store/singleProdReducer";
 
 export class Cart extends React.Component {
-  render() {
-  return(
-    <div>
-    {console.log("you're in the cart")}
-    <h1>YOU ARE IN THE CART</h1>
-    </div>
-
-  )
-  }
   // render() {
-  //   console.log("this.props", this.props);
-  //   console.log("this.state", this.state);
-  //   let addedItems = this.props.products.length ? (
-  //     this.props.products.map((product) => {
-  //       return (
-  //         <li className="collection-item avatar" key={product.id}>
-  //           <div className="item-img">
-  //             <img src={product.imageUrl} alt={product.imageUrl} className="" />
-  //           </div>
+  // return(
+  //   <div>
+  //   {console.log("you're in the cart")}
+  //   <h1>YOU ARE IN THE CART</h1>
+  //   </div>
 
-  //           <div className="item-desc">
-  //             <span className="title">{product.name}</span>
-  //             <p>{product.description}</p>
-  //             <p>
-  //               <b>Price: {product.price}$</b>
-  //             </p>
-  //             <p>
-  //               <b>Quantity: {product.quantity}</b>
-  //             </p>
-  //             {/* <div className="add-remove">
-  //                                 <Link to="/cart"><i className="material-icons">arrow_drop_up</i></Link>
-  //                                 <Link to="/cart"><i className="material-icons">arrow_drop_down</i></Link>
-  //                             </div> */}
-  //             <button className="waves-effect waves-light btn pink remove">
-  //               Remove
-  //             </button>
-  //           </div>
-  //         </li>
-  //       );
-  //     })
-  //   ) : (
-  //     <p>Nothing.</p>
-  //   );
-  //   return (
-  //     <div className="container">
-  //       <div className="cart">
-  //         <h5>You have ordered:</h5>
-  //         <ul className="collection">{addedItems}</ul>
-  //       </div>
-  //     </div>
-  //   );
+  // )
   // }
+  render() {
+    console.log("this.props", this.props);
+    console.log("this.state", this.state);
+    let addedItems = this.props.products.length ? (
+      this.props.products.map((product) => {
+        return (
+          <li className="collection-item avatar" key={product.id}>
+            <div className="item-img">
+              <img src={product.imageUrl} alt={product.imageUrl} className="" />
+            </div>
+
+            <div className="item-desc">
+              <span className="title">{product.name}</span>
+              <p>{product.description}</p>
+              <p>
+                <b>Price: {product.price}$</b>
+              </p>
+
+              {/* <div className="add-remove">
+                                  <Link to="/cart"><i className="material-icons">arrow_drop_up</i></Link>
+                                  <Link to="/cart"><i className="material-icons">arrow_drop_down</i></Link>
+                              </div> */}
+              <button className="waves-effect waves-light btn pink remove">
+                Remove
+              </button>
+            </div>
+          </li>
+        );
+      })
+    ) : (
+      <p>Nothing.</p>
+    );
+    return (
+      <div className="container">
+        <div className="cart">
+          <h5>You have ordered:</h5>
+          <ul className="collection">{addedItems}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-  console.log("state", state)
+  console.log("state", state);
   return {
     products: state.addedProducts,
   };
--- client/store/singleProdReducer.js
+++ client/store/singleProdReducer.js
@@ -1,42 +1,37 @@
-import axios from 'axios'
+import axios from "axios";
 
 // ACTION TYPE
-const SET_PRODUCT = 'SET_PRODUCT'
-
+const SET_PRODUCT = "SET_PRODUCT";
 
 //ACTION CREATOR
 export const setProduct = (product) => {
   return {
     type: SET_PRODUCT,
-    product
-  }
-}
-
-
+    product,
+  };
+};
 
 //THUNK -
 export const getProduct = (id) => {
   return async (dispatch) => {
     try {
+      const response = await axios.get(`/api/products/${id}`);
 
-      const response = await axios.get(`/api/product/${id}`)
-
-  
-      const data = response.data
-      dispatch(setProduct(data))
+      const data = response.data;
+      dispatch(setProduct(data));
     } catch (err) {
-      console.log (err)
+      console.log(err);
     }
-  }
-}
+  };
+};
 
 // REDUCER
-const initialState = []
-export default function productReducer (state = initialState, action) {
-  switch(action.type) {
+const initialState = [];
+export default function productReducer(state = initialState, action) {
+  switch (action.type) {
     case SET_PRODUCT:
-      return action.product
+      return action.product;
     default:
-      return state
+      return state;
   }
 }
