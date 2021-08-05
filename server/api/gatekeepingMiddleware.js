b716ed03ca843cfd4cf825dfc24fc344db4c4aba
Jesslenn <jessica.lennick@gmail>
2021-08-05 16:40:34 -0400

    fixed middleware bug

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -7,7 +7,7 @@ try {
   // give them a token when they log in, and it gets passed back and forth every time there is a request
   const token = req.headers.authorization;
   // find user if they're valid
-  const user = await User.findByToken
+  const user = await User.findByToken(token);
   // if successful, we can make the user avail and send to next path 
   req.user = user;
   next()
